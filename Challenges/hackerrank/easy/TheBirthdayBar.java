e6e16d92653cd36625b46d34d4ee939ab976b76b
Vitor Oliveira <vitinhocastro2@gmail.com>
2025-03-11 01:45:53 -0300

    HackerRank challenges

---
+++ Challenges/hackerrank/easy/TheBirthdayBar.java
@@ -0,0 +1,15 @@
+package Challenges.hackerrank.easy;
+
+import java.util.List;
+
+public class TheBirthdayBar {
+
+    public static int birthday(List<Integer> s, int d, int m) {
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(birthday(List.of(1, 2, 1, 3, 2), 4, 2));
+    }
+}
