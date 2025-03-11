5c7d9ec523df59a4f4de4e2999239ad97e233da2
Vitor Oliveira <vitinhocastro2@gmail.com>
2025-03-11 02:43:48 -0300

    HackerRank challenges

---
+++ Challenges/hackerrank/easy/MigratoryBirds.java
@@ -0,0 +1,15 @@
+package Challenges.hackerrank.easy;
+
+import java.util.List;
+
+public class MigratoryBirds {
+
+    public static int migratoryBirds(List<Integer> arr) {
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(migratoryBirds(List.of(1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4)));
+    }
+}
