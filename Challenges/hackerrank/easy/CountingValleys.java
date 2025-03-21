1c2720ffc6386627abfa3c1d66b881c4fcb0f493
Vitor Oliveira <vitinhocastro2@gmail.com>
2025-03-21 00:58:56 -0300

    HackerRank Challenges

---
+++ Challenges/hackerrank/easy/CountingValleys.java
@@ -0,0 +1,16 @@
+package Challenges.hackerrank.easy;
+
+public class CountingValleys {
+
+    public static int countingValleys(int steps, String path) {
+        int valleysTraversed = 0;
+
+
+
+        return valleysTraversed;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countingValleys(8, "DDUUUUDD"));
+    }
+}
